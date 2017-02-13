ac3bc8b1bdba1837940f2fb926ff4e8958aadf42
FractalTeam <fractal_team@hotmail.com>
2017-02-13 12:56:31 -0600

    Add files via upload

---
+++ Licenciado.java
@@ -0,0 +1,66 @@
+//
+package proyectolibreriacontactosfractal;
+
+public class Licenciado extends Contacto
+
+{
+
+	protected static String telefonooficina="";
+	protected static String horarioatencioninicio="";
+	protected static String horarioatencionfin="";
+
+	public Licenciado()
+	
+	{
+		super();
+	}
+
+	public Licenciado(String nombre,String apellidopaterno, String apellidomaterno,
+		String mediocontacto,int persona, String telefonooficina,
+		String horarioatencioninicio, String horarioatencionfin)
+	{
+		super(nombre,apellidopaterno,apellidomaterno,mediocontacto,idpersonal);
+		this.telefonooficina=telefonooficina;
+		this.horarioatencioninicio=horarioatencioninicio;
+		this.horarioatencionfin=horarioatencionfin;
+	}
+
+	public void setTelefonoOficina(String telefonooficina)
+	
+	{
+		this.telefonooficina=telefonooficina;
+	}
+
+	public void setHorarioAtencionInicio(String horarioatencioninicio)
+	
+	{
+		this.horarioatencioninicio=horarioatencioninicio;
+	}
+
+	public void setHorarioAtencionFin(String horarioatencionfin)
+	
+	{
+		this.horarioatencionfin=horarioatencionfin;
+	}
+
+	public String getTelefonoOficina()
+	
+	{
+		return this.telefonooficina;
+	}
+
+	public String getHorarioAtencionInicio()
+
+	{
+		return this.horarioatencioninicio;
+	}
+
+	public String getHorarioAtencionFin()
+
+	{
+		return this.horarioatencionfin;
+	}
+
+
+	
+}
